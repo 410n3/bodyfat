afbffc30036b1c336bf105f04bea83279f926bf5
Maneesh Radhakrishnan <43547211+410n3@users.noreply.github.com>
2023-02-26 16:10:06 +0530

    Update bodyfat_percentage.py

--- bodyfat_percentage.py
+++ bodyfat_percentage.py
@@ -103,8 +103,8 @@ def main():
     def insert_data(uid, email, Age, Weight, Height, bmi1, bmr1, bf2, bf1):
         sheet_url = st.secrets["private_gsheets_url"]
         conn.execute(
-        f'INSERT INTO "{sheet_url}" (uid, email, age, weight, height, bmi1, bmr1, bf2, bf1) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
-        (uid, email, age, weight, height, bmi1, bmr1, bf2, bf1))
+        f'INSERT INTO "{sheet_url}" (uid, email, Age, Weight, Height, bmi1, bmr1, bf2, bf1) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
+        (uid, email, Age, Weight, Height, bmi1, bmr1, bf2, bf1))
     def validate_email(email):
         # A simple regex to validate email format
         import re
